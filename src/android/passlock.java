b45f1e1ae0834f4c22a3831bef61e921ee540c07
Sean Nicholls <sean.nicholls@feedhenry.com>
2015-02-27 10:57:56 +0000

    removed redundant code

--- src/android/passlock.java
+++ src/android/passlock.java
@@ -8,10 +8,6 @@ import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import android.content.ContentResolver;
-import android.content.Context;
-import android.provider.Settings;
-
 import android.app.KeyguardManager;
 import android.app.Activity;
 
@@ -19,7 +15,6 @@ import android.app.Activity;
 public class passlock extends CordovaPlugin {
 
     private Activity _activity;
-    public CallbackContext callbackContext;
 
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
@@ -29,11 +24,9 @@ public class passlock extends CordovaPlugin {
 
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         
-        this.callbackContext = callbackContext;
+        if(action.equals("status")) {
 
-        if (action.equals("status")) {
-
-            int result = -1;
+            int result = -1; // unknown status 
 
             KeyguardManager keyguardManager = (KeyguardManager)_activity.getSystemService(_activity.KEYGUARD_SERVICE);
 
@@ -50,14 +43,6 @@ public class passlock extends CordovaPlugin {
 
         return false;
 
-    }
-
-    private void echo(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
     }
 
 }
